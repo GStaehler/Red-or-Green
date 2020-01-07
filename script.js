aed00801a3488f7a43cdc36097a30f1091cfb61b
gauthier.staehler <gauthier.staehler@uha.fr>
2020-01-07 11:47:05 +0100

    Clean and disable text highlight

--- script.js
+++ script.js
@@ -3,50 +3,49 @@ var myArray = [
   "lightgreen"
 ];
 
-function gameover() {
+function gameOver() {
 	document.body.style.backgroundColor = "lightblue";
 	document.getElementById("points").textContent = "Retry? Press Enter";
 	game();
 }
 
 function game() {
-	var points = 1;
+	let points = 1;
 	document.onkeydown = function (e) {
-		var keyCode = e.keyCode;
-		if (keyCode == 13) {
-			var randomItem = myArray[Math.floor(Math.random() * myArray.length)];
+		let keyCode = e.keyCode;
+		if (keyCode === 13) {
+			let randomItem = myArray[Math.floor(Math.random() * myArray.length)];
 			document.body.style.backgroundColor = randomItem;
 			document.getElementById("points").textContent = 0;
 			document.onkeydown = function (e) {
-				var keyCode = e.keyCode;
-				if (keyCode == 37) {
+				let keyCode = e.keyCode;
+				if (keyCode === 37) {
 					document.getElementById("left").focus();
 				}
-				if (keyCode == 39) {
+				if (keyCode === 39) {
 					document.getElementById("right").focus();
 				}
 			};
-
 			document.onkeyup = function (e) {
-				var keyCode = e.keyCode;
-				if (keyCode == 37) {
+				let keyCode = e.keyCode;
+				if (keyCode === 37) {
 					document.getElementById("left").blur();
 					if (document.body.style.backgroundColor === "pink") {
 						document.getElementById("points").textContent = points++;
-						var randomItem = myArray[Math.floor(Math.random() * myArray.length)];
+						let randomItem = myArray[Math.floor(Math.random() * myArray.length)];
 						document.body.style.backgroundColor = randomItem;
 					} else {
-						gameover();
+						gameOver();
 					}
 				}
-				if (keyCode == 39) {
+				if (keyCode === 39) {
 					document.getElementById("right").blur();
 					if (document.body.style.backgroundColor === "lightgreen") {
 						document.getElementById("points").textContent = points++;
-						var randomItem = myArray[Math.floor(Math.random() * myArray.length)];
+						let randomItem = myArray[Math.floor(Math.random() * myArray.length)];
 						document.body.style.backgroundColor = randomItem;
 					} else {
-						gameover();
+						gameOver();
 					}
 				}
 			};
